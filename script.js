9174c76d16462099f4d802479ed2365a94176488
souvik500 <120314952+souvik500@users.noreply.github.com>
2023-06-18 18:24:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+//your JS code here. If required.
+var browserInfoElement = document.getElementById("browser-info");
+var browserName = navigator.appName;
+var version = navigator.appVersion;
+browserInfoElement.textContent = "You are using " + browserName + " version " + version;
